3a9119212b64b65255cf00ba1c70fecbba799ad2
rvnSenkuuu <tolu_48@hotmail.fr>
2025-12-16 18:30:14 +0100

    init commit, pushing run script

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Variables
+IMAGE="rainfall.qcow2"
+ISO="RainFall.iso"
+ISO_URL="https://cdn.intra.42.fr/isos/RainFall.iso"
+IMAGE_SIZE="2G"
+
+# Création de l'image si elle n'existe pas
+if [ ! -f "$IMAGE" ]; then
+    echo "[+] Création de l'image $IMAGE ($IMAGE_SIZE)"
+    qemu-img create -f qcow2 "$IMAGE" "$IMAGE_SIZE"
+else
+    echo "[=] L'image $IMAGE existe déjà"
+fi
+
+# Téléchargement de l'ISO si elle n'existe pas
+if [ ! -f "$ISO" ]; then
+    echo "[+] Téléchargement de $ISO"
+    wget "$ISO_URL" -O "$ISO"
+else
+    echo "[=] L'ISO $ISO existe déjà"
+fi
+
+# Lancement de la VM
+echo "[+] Lancement de Rainfall"
+qemu-system-x86_64 \
+    -m 1024 \
+    -hda "$IMAGE" \
+    -cdrom "$ISO" \
+    -boot d \
+    -enable-kvm \
+    -net nic \
+    -net user,hostfwd=tcp::4243-:4242
