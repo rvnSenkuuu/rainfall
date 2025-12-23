7bafcfce4d43492940c4f8ad2153ff7b9e6ba21b
rvnSenkuuu <tolu_48@hotmail.fr>
2025-12-23 18:49:45 +0100

    add level4 source file

---
+++ level4/ressources/source.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	m;
+
+void	p(char *buffer)
+{
+	printf(buffer);
+}
+
+void	n(void)
+{
+	char	buffer[512];
+
+	fgets(buffer, 512, stdin);
+	p(buffer);
+	if (m == 0x1025544)
+		system("/bin/cat /home/user/level5/.pass");
+}
+
+int	main(void)
+{
+	n();
+	return 0;
+}
