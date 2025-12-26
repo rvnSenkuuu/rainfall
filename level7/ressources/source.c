6772b923a6a21cf75fadf567401243d0b7f34e26
rvnSenkuuu <tolu_48@hotmail.fr>
2025-12-26 18:08:16 +0100

    typo source file

--- level7/ressources/source.c
+++ level7/ressources/source.c
@@ -8,7 +8,7 @@ char	c[68];
 typedef struct {
 	int	x;
 	char	*buffer;
-} Foo;
+} foo;
 
 void	m(void)
 {
@@ -18,17 +18,17 @@ void	m(void)
 
 int	main(int argc, char **argv)
 {
-	Foo	*a, *b;
+	foo	*a, *b;
 	void	*buffer;
 	FILE	*stream;
 	
-	a = (Foo *)malloc(8);
+	a = (foo *)malloc(8);
 	a->x = 1;
 	buffer = malloc(8);
 	a->buffer = buffer;
 
-	b = (Foo *)malloc(8);
-	b->x = 1;
+	b = (foo *)malloc(8);
+	b->x = 2;
 	buffer = malloc(8);
 	b->buffer = buffer;
 
@@ -37,5 +37,6 @@ int	main(int argc, char **argv)
 	stream = fopen("/home/user/level8/.pass", "r");
 	fgets(c, 68, stream);
 	puts("~~");
+
 	return 0;
 }
